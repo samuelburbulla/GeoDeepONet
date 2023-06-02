Setup Poisson test case and fix jacobian transpose.

geodeeponet/pde.py
@@ -1,8 +1,7 @@
 import abc
 import torch
 import torch.autograd
-from geodeeponet.vectorcalculus import VectorCalculus
-from torch.autograd import grad
+from geodeeponet.deriv import grad, div
 
 class PDE(abc.ABC):
     """Abstract base class for partial differential equations."""
@@ -69,6 +68,9 @@ class Poisson(PDE):
             points (torch.Tensor): The points sampling the domain.
 
         """
+        self.dirichlet_indices = []
+        self.dirichlet_values = torch.tensor([])
+
         for i, x in enumerate(points):
             if self.bc.is_dirichlet(x):
                 self.dirichlet_indices += [i]
@@ -89,9 +91,8 @@ class Poisson(PDE):
 
         """
         # Compute derivatives
-        vc = VectorCalculus(points, jacobians)
-        gradu = vc.grad(u)
-        laplace_u = vc.div(gradu)
+        gradu = grad(u, points, jacobians)
+        laplace_u = div(gradu, points, jacobians)
 
         # Evaluate source term
         q = self.source(points)
@@ -105,15 +106,15 @@ class Poisson(PDE):
         boundary_loss = ((u_boundary - u_dirichlet)**2).mean()
 
         # Compute Neumann boundary loss (grad(u) * n = 0)
-        num_points = points.shape[0]
-        for i in range(num_points):
-            x = points[i, :]
-            if self.bc.is_neumann(x):
-                # Compute global normal vector
-                n = torch.as_tensor(jacobians[0, i] @ self.bc.normal(x), dtype=torch.float32)
-                # Compute normal derivative
-                gradu_n = gradu[:, :, i, :] @ n
-                boundary_loss += ((gradu_n)**2).mean()
+        # num_points = points.shape[0]
+        # for i in range(num_points):
+        #     x = points[i, :]
+        #     if self.bc.is_neumann(x):
+        #         # Compute global normal vector
+        #         n = torch.as_tensor(jacobians[0, i] @ self.bc.normal(x), dtype=torch.float32)
+        #         # Compute normal derivative
+        #         gradu_n = gradu[:, :, i, :] @ n
+        #         boundary_loss += ((gradu_n)**2).mean()
         
         return inner_loss, boundary_loss
 

geodeeponet/train.py
@@ -24,7 +24,7 @@ def compute_losses(model, pde, global_collocation_points, loss_points, jacobians
 
 
 def train_model(geom, model, collocation_points, phis, pde, 
-                num_inner_points=512, num_boundary_points=128,
+                num_inner_points=128, num_boundary_points=128,
                 tolerance=1e-5, steps=1000, print_every=1, plot_phis=False):
     """Trains a physics-informed GeoDeepONet model.
 
@@ -53,10 +53,9 @@ def train_model(geom, model, collocation_points, phis, pde,
     )
 
     # Get global collocation
-    global_collocation_points = []
-    for phi in phis:
-        global_collocation_points += [phi.inv(collocation_points)]
-    global_collocation_points = torch.stack(global_collocation_points)
+    global_collocation_points = torch.stack([
+        phi.inv(collocation_points) for phi in phis
+    ])
 
     i = 0
     for i in range(steps):
@@ -70,12 +69,11 @@ def train_model(geom, model, collocation_points, phis, pde,
         pde.setup_bc(loss_points)
 
         # Compute Jacobians
-        jacobians = []
-        for phi in phis:
-            global_loss_points = phi.inv(loss_points)
-            jac = torch.autograd.functional.jacobian(phi, global_loss_points)
-            jacobians += [jac.sum(axis=2)] # type: ignore
-        jacobians = torch.stack(jacobians)
+        jacobians = torch.stack([
+            torch.autograd.functional.jacobian(phi, phi.inv(loss_points))
+              .sum(axis=2).transpose(-1, -2) # type: ignore
+            for phi in phis
+        ])
 
         # Define closure
         def closure():
@@ -104,6 +102,7 @@ def train_model(geom, model, collocation_points, phis, pde,
 
             if train_loss < tolerance and train_boundary < tolerance:
                 break
+    
     print(" - not converged!" if i+1 == steps else " - done")
 
     # Plot solutions on tensorboard

test/poisson_test.py
@@ -1,62 +1,66 @@
 """Test physics-informed GeoDeepONet using an analytical solution of the Poisson problem."""
+import unittest
 import geodeeponet as gdn
 from torch import sin, pi
 import numpy as np
 
-# Parameters
-dim = 2
-num_collocation_points = 2**dim
-branch_width = 1
-trunk_width = 128
 
-# Domain
-geom = gdn.geometry.UnitCube(dim)
-collocation_points = geom.uniform_points(num_collocation_points)
+class TestStringMethods(unittest.TestCase):
 
-# Transformation
-phi = gdn.transformation.Affine(
-    A=np.array([[3., 0.], [0., 1.]]),
-    b=np.array([0., 0.])
-)
-# phi = gdn.transformation.PolarCoordinates(
-#     r_min=0.5, r_max=1.0,
-#     theta_min=0.0, theta_max=.5*pi,
-# )
+    def test_poisson(self):
+        # Parameters
+        dim = 2
+        num_collocation_points = 2**dim
+        branch_width = 1
+        trunk_width = 256
 
-# Exact solution
-def exact_solution(x):
-    return sin(2 * pi * x[..., 0]) * sin(2 * pi * x[..., 1]) / (8 * pi**2)
+        # Domain
+        geom = gdn.geometry.UnitCube(dim)
+        collocation_points = geom.uniform_points(num_collocation_points)
 
-# Source
-def source(x):
-    return sin(2 * pi * x[..., 0]) * sin(2 * pi * x[..., 1])
+        # Transformation
+        phi = gdn.transformation.Affine(
+            A=np.array([[1., 0.], [1., 1.]]),
+            b=np.array([0., 0.]),
+        )
 
-# Boundary condition
-bc = gdn.bc.UnitCubeDirichletBC({
-    w: (lambda x: exact_solution(phi.inv(x))) for w in ["left", "right", "top", "bottom"]
-})
+        # Exact solution
+        def exact_solution(x):
+            return sin(2 * pi * x[..., 0]) * sin(2 * pi * x[..., 1]) / (8 * pi**2)
 
-# Define PDE
-pde = gdn.pde.Poisson(bc, lambda x: source(phi.inv(x)))
+        # Source
+        def source(x):
+            return sin(2 * pi * x[..., 0]) * sin(2 * pi * x[..., 1])
 
-# Setup DeepONet
-model = gdn.deeponet.GeoDeepONet(
-    branch_width=branch_width,
-    trunk_width=trunk_width,
-    num_collocation_points=len(collocation_points),
-    dimension=geom.dim,
-    outputs=pde.outputs,
-)
+        # Boundary condition
+        bc = gdn.bc.UnitCubeDirichletBC({
+            w: (lambda x: exact_solution(phi.inv(x)))
+            for w in ["left", "right", "bottom", "top"]
+        })
 
-# Train model
-gdn.train.train_model(
-    geom, model, collocation_points, [phi], pde
-)
+        # Define PDE
+        pde = gdn.pde.Poisson(bc, lambda x: source(phi.inv(x)))
 
-# Plot solution for one sample transformation
-gdn.plot.plot_solution(geom, model, collocation_points, phi, writer="show")
+        # Setup DeepONet
+        model = gdn.deeponet.GeoDeepONet(
+            branch_width=branch_width,
+            trunk_width=trunk_width,
+            num_collocation_points=len(collocation_points),
+            dimension=geom.dim,
+            outputs=pde.outputs,
+        )
 
-# Compute error
-l2 = gdn.error.l2(geom, model, collocation_points, phi, exact_solution)
-print(f"L2-Error: {l2:.3e}")
-assert l2 < 1e-3
+        # Train model
+        gdn.train.train_model(geom, model, collocation_points, [phi], pde)
+
+        # Plot solution for one sample transformation
+        gdn.plot.plot_solution(geom, model, collocation_points, phi, writer="show")
+
+        # Compute error
+        l2 = gdn.error.l2(geom, model, collocation_points, phi, exact_solution)
+        print(f"L2-Error: {l2:.3e}")
+        assert l2 < 1e-3
+
+
+if __name__ == '__main__':
+    unittest.main()